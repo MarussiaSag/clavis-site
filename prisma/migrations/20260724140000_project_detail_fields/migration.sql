a7ace8d4c259c6f3c9b36bca1d6a1291326179e2
Maria Derbeneva <sagadullaevam@gmail.com>
2026-07-24 14:21:28 +0300

    added migration

---
+++ prisma/migrations/20260724140000_project_detail_fields/migration.sql
@@ -0,0 +1,13 @@
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN "styleLabel" TEXT;
+ALTER TABLE "Project" ADD COLUMN "layoutLabel" TEXT;
+ALTER TABLE "Project" ADD COLUMN "aboutSummary" TEXT;
+ALTER TABLE "Project" ADD COLUMN "quote" TEXT;
+ALTER TABLE "Project" ADD COLUMN "quoteAttribution" TEXT;
+ALTER TABLE "Project" ADD COLUMN "aboutBody" TEXT;
+ALTER TABLE "Project" ADD COLUMN "aboutSideBody" TEXT;
+ALTER TABLE "Project" ADD COLUMN "aboutImage" TEXT;
+ALTER TABLE "Project" ADD COLUMN "roomsJson" TEXT;
+ALTER TABLE "Project" ADD COLUMN "materialsJson" TEXT;
+ALTER TABLE "Project" ADD COLUMN "teamJson" TEXT;
+ALTER TABLE "Project" ADD COLUMN "virtualTourUrl" TEXT;
